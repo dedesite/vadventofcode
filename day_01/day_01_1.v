8a9edf3562a9c39a89a9638bb76797feb4e87392
Andréas Livet <andreas.livet@gmail.com>
2020-12-01 21:41:02 +0100

    day_01_1 : remove variable and better print

--- day_01/day_01_1.v
+++ day_01/day_01_1.v
@@ -2,16 +2,13 @@ import os
 
 fn main() {
 	numbers := os.read_lines('input') ?
-	mut start_key := 0
-	for num in numbers {
+	for key, num in numbers {
 		num_left := num.int()
-		start_key++
-		for n in numbers[start_key..numbers.len] {
+		for n in numbers[key+1..numbers.len] {
 			num_right := n.int()
 			if num_left + num_right == 2020 {
-				println(num_left)
-				println(num_right)
-				println(num_left * num_right)
+				println("$num_left + $num_right = ${num_left + num_right}")
+				println("$num_left * $num_right = ${num_left * num_right}")
 				break
 			}
 		}
