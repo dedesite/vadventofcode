8db7888bfa6d2f97e8cccc17da14180944332f0d
Andréas Livet <andreas.livet@gmail.com>
2020-12-06 08:35:46 +0100

    day_06_1 : with remove_duplicate

--- day_06/day_06_1.v
+++ day_06/day_06_1.v
@@ -1,16 +1,20 @@
 import os
 
-answers_content := os.read_file('input') ?
-answers_group := answers_content.split('\n\n')
-splited_answers := answers_group.map(it.replace('\n', '')).map(it.split(''))
-mut yes_count := 0
-for group in splited_answers {
-	mut single_letters := []string{}
-	for letter in group {
-		if letter !in single_letters {
-			single_letters.push(letter)
+fn remove_duplicates(s string) string {
+	mut single_letters := ''
+	for letter in s {
+		if letter.str() !in single_letters {
+			single_letters += letter.str()
 		}
 	}
-	yes_count += single_letters.len
+	return single_letters
+}
+
+answers_content := os.read_file('input') ?
+answers_group := answers_content.split('\n\n')
+answers := answers_group.map(it.replace('\n', ''))
+mut yes_count := 0
+for group in answers {
+	yes_count += remove_duplicates(group).len
 }
 println('The sum of the yes anwsers is : $yes_count')
