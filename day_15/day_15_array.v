5876b4f4476f986a832874fec68d6efbdffd80f9
Andréas Livet <andreas.livet@gmail.com>
2020-12-15 22:50:58 +0100

    day 15 : array solution tentative

---
+++ day_15/day_15_array.v
@@ -0,0 +1,31 @@
+const (
+	n  = 30000000
+	k  = 6
+	i0 = k - 1
+)
+
+mut data := []int{ cap: n }
+input := '8,13,1,0,18,9'
+for i, num in input.split(',') {
+	data[i] = num.int()
+}
+
+mut rec := [n]int{}
+mut spoken := 0
+for i, d in data[0..k - 1] {
+	rec[d] = i + 1
+}
+for i := i0; i + 2 <= n; i++ {
+	n := data[i]
+	w := rec[n]
+	if w == 0 {
+		rec[n] = i + 1
+		spoken = 0
+		data[i + 1] = spoken
+	} else {
+		spoken = i + 1 - w
+		data[i + 1] = spoken
+		rec[n] = i + 1
+	}
+}
+println('spoken $spoken')
