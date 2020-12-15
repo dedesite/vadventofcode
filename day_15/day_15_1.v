1f63090293247d722eeffefa818626f612ea2618
Andréas Livet <andreas.livet@gmail.com>
2020-12-15 22:26:07 +0100

    day 15 part 1 & 2

--- day_15/day_15_1.v
+++ day_15/day_15_1.v
@@ -1,15 +1,22 @@
-input := "8,13,1,0,18,9"
-
-struct SpokenNumber {
-	number int
-	last_position int
-}
-
-starting_numbers = input.split(',')
+input := '8,13,1,0,18,9'
+starting_numbers := input.split(',')
 mut turns := 0
-mut spoken_number_list := map[string]int
-for turns < 2020 {
+mut spoken_number_list := map[string]int{}
+mut last_spoken_number := 0
+for turns = 1; turns < 30000000; turns++ {
 	if turns < starting_numbers.len {
-		spoken_number_list[starting_numbers[turns]] = turns
+		spoken_number_list[starting_numbers[turns-1]] = turns
+		last_spoken_number = starting_numbers[turns].int()
+	} else {
+		if last_spoken_number.str() in spoken_number_list {
+			last_turn := spoken_number_list[last_spoken_number.str()]
+			spoken_number_list[last_spoken_number.str()] = turns
+			last_spoken_number = turns - last_turn
+		} else {
+			spoken_number_list[last_spoken_number.str()] = turns
+			last_spoken_number = 0
+		}
+
 	}
 }
+println('The 2020th number spoken will be $last_spoken_number')
